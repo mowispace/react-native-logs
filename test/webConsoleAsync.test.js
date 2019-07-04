5fea36b11335acf88b21b2f91dff63f23c4feca7
Alessandro Bottamedi <a.bottamedi@onubo.com>
2019-07-04 11:15:23 +0200

    Separe test for different logs type

--- test/webConsoleAsync.test.js
+++ test/webConsoleAsync.test.js
@@ -10,23 +10,31 @@ test("The log function should return true", () => {
   expect(log.trace("message")).toBe(true)
 })
 
-test("The log function should print string, objects, functions in console", done => {
+test("The log function should print string console", done => {
   var log = rnlogs.logger.createLogger({ transport: transport })
-  function cb1() {
+  function cb() {
     expect("called").toBe("called")
     done()
   }
-  log.trace("message", cb1)
-  function cb2() {
-    expect("called").toBe("called")
-    done()
-  }
-  log.trace({ message: "message" }, cb2)
-  function cb3() {
+  log.trace("message", cb)
+})
+
+test("The log function should print functions console", done => {
+  var log = rnlogs.logger.createLogger({ transport: transport })
+  function cb() {
     expect("called").toBe("called")
     done()
   }
   log.trace(() => {
     return true
-  }, cb3)
+  }, cb)
+})
+
+test("The log function should print objects console", done => {
+  var log = rnlogs.logger.createLogger({ transport: transport })
+  function cb() {
+    expect("called").toBe("called")
+    done()
+  }
+  log.trace({ message: "message" }, cb)
 })
