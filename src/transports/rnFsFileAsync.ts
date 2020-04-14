bdb6a89f383f4da6b888623f5143d6b34b00e596
Jelle Breuer <jelle@breuclan.nl>
2020-04-14 13:36:30 +0200

    Tiny fix
    Your reference RNFS before it is created, throws an error on my phone

--- src/transports/rnFsFileAsync.ts
+++ src/transports/rnFsFileAsync.ts
@@ -3,6 +3,12 @@ import { transportFunctionType } from '../index';
 declare var require: any;
 
 const rnFsFileAsync: transportFunctionType = (msg, level, options) => {
+  try {
+    var RNFS = require('react-native-fs');
+  } catch (error) {
+    console.error('Unable to load react-native-fs, try "yarn add react-native-fs"');
+    return true;
+  }
   /**
    * Control msg type
    * Here we use JSON.stringify so you can pass object, array, string, ecc...
@@ -27,14 +33,6 @@ const rnFsFileAsync: transportFunctionType = (msg, level, options) => {
   if (options && options.loggerPath) loggerPath = options.loggerPath;
 
   let output = `${dateTxt}${levelTxt}${stringMsg}\n`;
-
-  try {
-    var RNFS = require('react-native-fs');
-  } catch (error) {
-    console.error('Unable to load react-native-fs, try "yarn add react-native-fs"');
-    return true;
-  }
-
   var path = loggerPath + '/' + loggerName + '.txt';
 
   RNFS.appendFile(path, output, 'utf8')
