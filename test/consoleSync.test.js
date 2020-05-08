c7a3c7955028d43fb4d8caa6ad93672fc002e267
Baldur <baldur.gudbjornsson@gmail.com>
2020-05-08 15:36:51 +0100

    Added dataFormat transportOptions

--- test/consoleSync.test.js
+++ test/consoleSync.test.js
@@ -39,6 +39,46 @@ test("When set transportOptions={hideDate:false, hideLevel:false} and empty msg,
   expect(outputData).toBe("")
 })
 
+test("When set without dateformat should output local dateformat", () => {
+  var log = rnlogs.logger.createLogger({ transport: transport })
+  var outputData = ""
+  var storeLog = inputs => (outputData += inputs)
+  console["log"] = jest.fn(storeLog)
+  log.debug("message")
+  var pattern = /\d\d:\d\d:\d\d\ \| DEBUG \| message$/;
+  expect(outputData).toMatch(pattern)
+})
+
+test("When set transportOptions={dateFormat:'local'}, should output toLocalString dateformat", () => {
+  var log = rnlogs.logger.createLogger({ transport: transport, transportOptions:{ dateFormat:'local' } })
+  var outputData = ""
+  var storeLog = inputs => (outputData += inputs)
+  console["log"] = jest.fn(storeLog)
+  log.debug("message")
+  var pattern = /\d\d:\d\d:\d\d\ \| DEBUG \| message$/;
+  expect(outputData).toMatch(pattern)
+})
+
+test("When set transportOptions={dateFormat:'utc'}, should output toUTCString dateformat", () => {
+  var log = rnlogs.logger.createLogger({ transport: transport, transportOptions:{ dateFormat:'utc' } })
+  var outputData = ""
+  var storeLog = inputs => (outputData += inputs)
+  console["log"] = jest.fn(storeLog)
+  log.debug("message")
+  var pattern = /\d\d:\d\d:\d\d GMT \| DEBUG \| message$/
+  expect(outputData).toMatch(pattern)
+})
+
+test("When set transportOptions={dateFormat:'iso'}, should output toISOString dateformat", () => {
+  var log = rnlogs.logger.createLogger({ transport: transport, transportOptions:{ dateFormat:'iso' } })
+  var outputData = ""
+  var storeLog = inputs => (outputData += inputs)
+  console["log"] = jest.fn(storeLog)
+  log.debug("message")
+  var pattern = /T\d\d:\d\d:\d\d\.\d\d\dZ \| DEBUG \| message$/;
+  expect(outputData).toMatch(pattern)
+})
+
 test("The log function should print expected output", () => {
   var log = rnlogs.logger.createLogger({ transport: transport, transportOptions:{ hideDate:true } })
   var outputData = ""
